6361ce882c0f0e90a18645b85c6eedea5eb910a3
shahzaibkhan641 <shahzaibcuiatk@gmail.com>
2025-03-04 06:54:38 -0800

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,39 @@
+let name="Shahzaib Khan";
+console.log(name);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt(0));
+console.log(name.charAt(1));
+console.log(name.charAt(2));
+console.log(name.indexOf('A'));
+console.log(name.lastIndexOf('i'));
+console.log(name.includes('Shahzaib'));
+console.log(name.startsWith('Khan'));
+console.log(name.endsWith('Ali'));
+console.log(name.substring(4, 9));
+console.log(name.slice(-3));
+console.log(name.split('r'));
+console.log(name.replace('Shahzaib', 'Mr.'));
+
+//example of array methods
+let arr = ['Shahzaib', 'Khan', 'Ali', 'JavaScript', 'Developer',2025];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr.indexOf('Ali'));
+console.log(arr.lastIndexOf('Developer'));
+console.log(arr.includes('Java'));
+console.log(arr.join(''));
+console.log(arr.slice(2, 5));
+console.log(arr.splice(1, 2));
+console.log(arr.push('Developer'));
+console.log(arr.pop());
+console.log(arr.shift());
+console.log(arr.unshift('Khan'));
+console.log(arr.reverse());
+
+let arr2=['comsats', 'University'];
+console.log(arr.concat(arr2));
+   
